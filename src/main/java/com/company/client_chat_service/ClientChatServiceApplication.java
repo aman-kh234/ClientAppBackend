5466001090c198559474c3dea09efa5053c6e554
Aman Kharbanda <amankharbanda2002@gmail.com>
2025-11-29 01:40:20 +0530

    add files

---
+++ src/main/java/com/company/client_chat_service/ClientChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.client_chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/client_chat_service/ClientChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.client_chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
